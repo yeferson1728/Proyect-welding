913f54b99dbef1d5ad79a29bda50e4b67ef73d58
yeferson1728 <yefersonmonterrosa1728@mail.com>
2025-01-05 22:15:08 -0500

    primer commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// // Función para alternar la clase 'active' en el menú
+// function toggleMenu() {
+//   const navLinks = document.querySelector("nav ul");
+//   navLinks.classList.toggle("active"); // Muestra u oculta el menú
+
+//   // También activar el submenú de productos en pantallas pequeñas
+//   const productos = document.querySelector(".productos");
+//   productos.classList.toggle("active");
+// }
